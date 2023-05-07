e189552cbfa273e1fef6e25622e11894ec89f77f
Anastasiya Demidchik <iskhakova.anastasiya@gmail.com>
2023-05-07 20:28:23 +0200

    refactor: swap lines of code

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,25 +6,25 @@ const dirPathFile = path.join(__dirname);
 const writeableStream = fs.createWriteStream(dirPathFile + '/text.txt', { flags: 'w' });
 
 console.log('Введите текст (для завершения нажмите Ctrl + C или введите exit)');
+process.on('SIGINT', () => {
+  console.log('Работа завершена. Всего хорошего!');
+  process.exit();
+});
 
 const rl = readline.createInterface({
   input: process.stdin,
 });
 
 rl.on('line', (input) => {
-  writeableStream.write(input + '\n', (err) => {
-    if (err) {
-      console.log(err.message);
-    }
-  });
   if (input === 'exit') {
     console.log('Работа завершена. Всего хорошего!');
     writeableStream.end();
     process.exit();
   } else {
-    process.on('SIGINT', () => {
-      console.log('Работа завершена. Всего хорошего!');
-      process.exit();
+    writeableStream.write(input + '\n', (err) => {
+      if (err) {
+        console.log(err.message);
+      }
     });
   }
 });
